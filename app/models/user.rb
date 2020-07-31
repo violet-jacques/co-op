64e884556ab3545b4eda14ff56f81536e93d6fd3
Violet Jacques <violet.jacques630@gmail.com>
2020-07-31 17:03:36 -0400

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
+end
