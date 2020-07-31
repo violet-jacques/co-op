48456a067cdfdb2a77af0fd157afa84d22f705cf
Violet Jacques <violet.jacques630@gmail.com>
2020-07-31 17:04:59 -0400

    add routes for devise registration and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: "api/users/sessions",
+    registrations: "api/users/registrations",
+  }  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
