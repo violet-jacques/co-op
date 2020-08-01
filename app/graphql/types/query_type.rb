3886dba837b400c71f93a033b1d1b5489900dc72
Violet Jacques <violet.jacques630@gmail.com>
2020-07-31 22:22:51 -0400

    all schemas require a query and mutation type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,4 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    field :test, String, null: true
+
+    def test
+      "hello world!"
+    end
   end
 end
