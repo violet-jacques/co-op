b4e4bafbee0ef20f1d9cc8703c40f51e70d1c5b9
Violet Jacques <violet.jacques630@gmail.com>
2020-07-31 23:34:47 -0400

    remove serialize method from user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,4 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
-
-  def serialize
-    UserSerializer.serialize(self)
-  end
 end
