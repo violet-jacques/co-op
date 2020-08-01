6d472f88d6956d4ecf18853c49ee15c300f0f57f
Violet Jacques <violet.jacques630@gmail.com>
2020-07-31 22:23:59 -0400

    ensures user emails are always unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
+  validates :email, uniqueness: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 
   def serialize
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe User do
   describe "validations" do
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should allow_value("email@addresse.foo").for(:email) }
     it { should_not allow_value("foo").for(:email) }
   end
