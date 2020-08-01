5957ecfdd27e0046c62d76f5759ab2a1e9347147
Violet Jacques <violet.jacques630@gmail.com>
2020-07-31 22:26:22 -0400

    remove devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: "api/users/sessions",
-    registrations: "api/users/registrations",
-  }  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/graphql", to: "graphql#execute"
 end
