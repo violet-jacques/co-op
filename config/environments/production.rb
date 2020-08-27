52f06e73f4e85b267d41e9067591f130f45c7041
Violet Jacques <violet.jacques630@gmail.com>
2020-08-26 22:19:13 -0400

    update all instances of old name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "co_op_production"
+  # config.active_job.queue_name_prefix = "we_are_production"
 
   config.action_mailer.perform_caching = false
 
