d3b201b462ae04111b6d728ee97da30016f31a01
Violet Jacques <violet.jacques630@gmail.com>
2020-08-26 23:13:10 -0400

    separate user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
+  include Validations::User
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   has_one :user_detail
 end
